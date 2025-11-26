9cbf18ea00def8829e44f63da803501c3aa15357
dsouzaryanaws <dsouzaryanaws@gmail.com>
2025-11-25 20:17:08 -0500

    feat: implement end-to-end request tracing with X-Ray (REL06-BP07)
    - Enable X-Ray tracing on Lambda function
    - Enable X-Ray tracing on API Gateway
    - Instrument boto3 DynamoDB client with X-Ray SDK
    - Add aws-xray-sdk dependency to Lambda requirements
    
    Fixes #15

--- python/apigw-http-api-lambda-dynamodb-python-cdk/stacks/apigw_http_api_lambda_dynamodb_python_cdk_stack.py
+++ python/apigw-http-api-lambda-dynamodb-python-cdk/stacks/apigw_http_api_lambda_dynamodb_python_cdk_stack.py
@@ -98,6 +98,7 @@ class ApigwHttpApiLambdaDynamodbPythonCdkStack(Stack):
             memory_size=1024,
             timeout=Duration.minutes(5),
             log_retention=logs.RetentionDays.ONE_YEAR,
+            tracing=lambda_.Tracing.ACTIVE,
         )
 
         # grant permission to lambda to write to demo table
@@ -130,5 +131,6 @@ class ApigwHttpApiLambdaDynamodbPythonCdkStack(Stack):
                     status=True,
                     user=True,
                 ),
+                tracing_enabled=True,
             ),
         )
